ff543c2902bd721c3bd2d000c1fca25bec5bcfa6
AlgoPorAi <20261ctb0100055@estudantes.ifpr.edu.br>
2026-04-07 18:45:23 +0000

    Aula 10 - 1004: produto simples

---
+++ beecrowd/1004-produto-simples.py
@@ -0,0 +1,19 @@
+'''
+Problema: BeeCrowd: 1004
+Data: 2026.4.7
+Estudante: Lorenzo Waselik
+'''
+# Objetivo: Multiplicar dois valores para obter um resultado
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Variáveis A e B (VAlores inteiros digitados)
+# Processamento: Multiplicar as variáveis A e B 
+#Saída: Mostrar "PROD = resultado"
+
+A = int(input())
+B = int(input())
+
+PROD = A * B
+
+#Saída
+print(f"PROD = {PROD}")
