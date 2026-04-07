3a9d8067c9444e9b542792cbae39906d596e4242
AlgoPorAi <20261ctb0100055@estudantes.ifpr.edu.br>
2026-04-07 18:34:01 +0000

    Aula 10 - 1003: soma simples

---
+++ beecrowd/1003.py
@@ -0,0 +1,21 @@
+'''
+Problema: BeeCrowd: 1003
+Data: 2026.4.7
+Estudante: Lorenzo Waselik
+'''
+# Objetivo: Somar dois valores inteiros e mostrar o resultado na variável "SOMA"
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Dois números inteiros em linhas separadas
+# Processamento: Somar A e B e armazenar na variável SOMA
+# Saída: Exibir como "SOMA = resultado"
+
+# Leitura das variáveis
+A = int(input())
+B = int(input())
+
+# Cálculo
+SOMA = A + B
+
+# Saída
+print(f"SOMA = {SOMA}")
