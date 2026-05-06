42efae7233e4ef2bf4d8a7401988e5d2f798aee1
AlgoPorAi <20261ctb0100055@estudantes.ifpr.edu.br>
2026-05-06 22:31:15 +0000

    Aula 11 - mes

---
+++ beecrowd/1052-mes.py
@@ -0,0 +1,38 @@
+'''
+Problema: BeeCrowd: 1052
+Data: 2026.04.30
+Estudante: Lorenzo Waselik
+'''
+# Objetivo: Digitar um valor inteiro e mostrar o nome do mês baseado em seu número
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Um valor inteiro (número do mês)
+# Processamento: Relacionar o número ao mês correspondente
+# Saída: Exibir apenas o nome do mês em inglês com a primeira letra maiúscula
+
+MES = int(input())
+
+if MES == 1:
+    print("January")
+elif MES == 2:
+    print("February")
+elif MES == 3:
+    print("March")
+elif MES == 4:
+    print("April")
+elif MES == 5:
+    print("May")
+elif MES == 6:
+    print("June")
+elif MES == 7:
+    print("July")
+elif MES == 8:
+    print("August")
+elif MES == 9:
+    print("September")
+elif MES == 10:
+    print("October")
+elif MES == 11:
+    print("November")
+elif MES == 12:
+    print("December")
