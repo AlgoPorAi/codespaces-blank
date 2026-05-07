9a51a604ecd843c7b076c6421f2613a960a8a8ca
AlgoPorAi <20261ctb0100055@estudantes.ifpr.edu.br>
2026-05-07 19:49:24 +0000

    Aula 13 - distancia

---
+++ beecrowd/1016-distancia.py
@@ -0,0 +1,17 @@
+'''
+Problema: BeeCrowd: 1016
+Data: 2026.05.07
+Estudante: Lorenzo Waselik
+'''
+# Objetivo: calcular quanto tempo leva em minutos parao Carro Y se distânciar do carro X
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Um número inteiro (km)
+# Processamento: realizar a operação Km / 2 para descobrir os minutos
+# Saída: mostrar apenas "resultado minutos"
+
+Km = int(input())
+
+tempo = Km * 2
+
+print(f"{tempo} minutos")
