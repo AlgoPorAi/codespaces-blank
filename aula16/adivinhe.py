d14f60a4304f5eb3fc625fe4d42daa650a27103f
AlgoPorAi <20261ctb0100055@estudantes.ifpr.edu.br>
2026-06-02 16:43:39 +0000

    Aula 16 - jogo adivinhe o número com 3 níveis

--- aula16/adivinhe.py
+++ aula16/adivinhe.py
@@ -7,16 +7,43 @@
 
 import random
 
-# 1) Sorteamos o número secreto entre 1 e 10
-numero_secreto = random.randint(1, 10)
 
-# 2) Pedimos um palpite (input devolve TEXTO; convertemos para inteiro)
-palpite = int(input("Digite um número de 1 a 10: "))
+def jogar(maximo, chances):
+    numero_secreto = random.randint(1, maximo)
+    acertou = False
 
-# 3) Comparamos o palpite com o número secreto
-if palpite == numero_secreto:
-    print("Acertou! O número era", numero_secreto)
-elif palpite < numero_secreto:
-    print("Muito baixo! Tente um número maior.")
-else:
-    print("Muito alto! Tente um número menor.")
+    while chances > 0 and not acertou:
+        palpite = int(input("Seu palpite (1 a " + str(maximo) + "): "))
+
+        if palpite == numero_secreto:
+            print("🎉 Acertou!")
+            acertou = True
+        elif palpite < numero_secreto:
+            print("📈 Muito baixo!")
+        else:
+            print("📉 Muito alto!")
+
+        chances = chances - 1 
+        print("Chances restantes:", chances)
+
+    return acertou
+
+niveis = [
+    ["Fácil", 10, 3],
+    ["Médio", 100, 5],
+    ["Impossível", 1000, 10],
+]
+
+print("Escolha o nível de dificuldade:")
+print("1 - Fácil      (1 a 10, 3 chances)")
+print("2 - Médio      (1 a 100, 5 chances)")
+print("3 - Impossível (1 a 1000, 10 chances)")
+opcao = int(input("Digite 1,2 ou 3: "))
+
+nivel = niveis [opcao - 1]
+
+print("Você escolheu o nível:", nivel[0])
+venceu = jogar(nivel[1], nivel[2])
+
+if not venceu:
+    print("💀 Fim de jogo! Tente um nível mais fácil. 😉")
